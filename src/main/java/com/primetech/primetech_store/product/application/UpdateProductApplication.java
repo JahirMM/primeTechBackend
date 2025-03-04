f3acbde53827d35912f36bc6eb00caa593619c28
JahirMM <jahirmachuca45@gmail.com>
2025-03-04 17:53:08 -0300

    correct update of information to maintain cameras if not changed category

--- src/main/java/com/primetech/primetech_store/product/application/UpdateProductApplication.java
+++ src/main/java/com/primetech/primetech_store/product/application/UpdateProductApplication.java
@@ -31,42 +31,41 @@ public class UpdateProductApplication {
     @Transactional
     public ProductDTO updateProductApplication(UUID productId, String email, ProductRequestDTO request) {
         User user = userService.findUserInformationByEmail(email);
-
         Product product = productService.findByProductIdAndSellerId(productId, user.getUserId());
+
+        String currentCategoryName = product.getCategory().getCategoryName();
+
         product.setName(request.getName());
         product.setDescription(request.getDescription());
         product.setBrand(request.getBrand());
         product.setStock(request.getStock());
         product.setPrice(request.getPrice().setScale(3, RoundingMode.HALF_UP));
 
-        Category category = categoryService.findCategoryByCategoryName(request.getCategory());
-        product.setCategory(category);
+        Category newCategory = categoryService.findCategoryByCategoryName(request.getCategory());
+
+        product.setCategory(newCategory);
 
         Product saveProduct = productService.saveProduct(product);
 
         Device device = deviceService.findDevicebyProductId(product.getProductId());
-        DeviceType deviceType = determineDeviceByCategory(category, device.getDeviceId());
+
+        String currentDeviceTypeName = device.getDeviceType().getTypeName();
+
+        DeviceType newDeviceType = determineDeviceByCategory(currentCategoryName, newCategory.getCategoryName(), device.getDeviceId(), currentDeviceTypeName);
 
         device.setProduct(product);
-        device.setDeviceType(deviceType);
+        device.setDeviceType(newDeviceType);
 
-        ProductDTO productDTO = new ProductDTO();
-        productDTO.setProductId(saveProduct.getProductId());
-        productDTO.setName(saveProduct .getName());
-        productDTO.setDescription(saveProduct .getDescription());
-        productDTO.setBrand(saveProduct .getBrand());
-        productDTO.setStock(saveProduct .getStock());
-        productDTO.setPrice(saveProduct .getPrice());
-        productDTO.setCategory(category.getCategoryName());
-        productDTO.setDeviceType(deviceType.getTypeName());
-        productDTO.setCreatedAt(saveProduct .getCreatedAt());
-        productDTO.setUpdatedAt(saveProduct.getUpdatedAt());
-
-        return productDTO;
+        return buildProductDTO(saveProduct, newCategory, newDeviceType);
     }
 
-    private DeviceType determineDeviceByCategory(Category category, UUID deviceId) {
-        switch (category.getCategoryName().toLowerCase()) {
+    private DeviceType determineDeviceByCategory(String oldCategory, String newCategory, UUID deviceId, String oldDeviceType) {
+        if (oldCategory.equalsIgnoreCase(newCategory)) {
+            // Si la categoría no cambió, no hacemos nada y retornamos el mismo DeviceType
+            return deviceTypeService.findDeviceTypeByTypeName(oldDeviceType);
+        }
+
+        switch (newCategory.toLowerCase()) {
             case "cellular":
             case "tablet":
                 deleteLaptopData(deviceId);
@@ -82,6 +81,21 @@ public class UpdateProductApplication {
                 deleteMobileDeviceData(deviceId);
                 return deviceTypeService.findDeviceTypeByTypeName("other");
         }
+    }
+
+    private ProductDTO buildProductDTO(Product product, Category category, DeviceType deviceType) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setProductId(product.getProductId());
+        productDTO.setName(product.getName());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setBrand(product.getBrand());
+        productDTO.setStock(product.getStock());
+        productDTO.setPrice(product.getPrice());
+        productDTO.setCategory(category.getCategoryName());
+        productDTO.setDeviceType(deviceType.getTypeName());
+        productDTO.setCreatedAt(product.getCreatedAt());
+        productDTO.setUpdatedAt(product.getUpdatedAt());
+        return productDTO;
     }
 
     private void deleteLaptopData(UUID deviceId) {
